7cb7b33713f2832debe57d47ab585f34e8ba1466
root <root@localhost.localdomain>
2019-05-14 11:21:25 -0400

    无用的错误提示,删掉.

--- wp-content/plugins/sqlite-integration/install.php
+++ wp-content/plugins/sqlite-integration/install.php
@@ -64,15 +64,6 @@ function wp_install($blog_title, $user_name, $user_email, $public, $deprecated =
 
 	wp_cache_flush();
 
-	if (isset($_SERVER['SERVER_SOFTWARE']) && stripos($_SERVER['SERVER_SOFTWARE'], 'apache') !== false || isset($_SERVER['SERVER_SIGNATURE']) && stripos($_SERVER['SERVER_SIGNATURE'], 'apache') !== false) {
-		;// Your server is Apache. Nothing to do more.
-	} else {
-		$server_message = sprintf('Your webserver doesn\'t seem to be Apache. So the database directory access restriction by the .htaccess file may not function. We strongly recommend that you should restrict the access to the directory %s in some other way.', FQDBDIR);
-		echo '<div style="position: absolute; margin-top: 350px; width: 700px; border: .5px dashed rgb(0, 0, 0);"><p style="margin: 10px;">';
-		echo $server_message;
-		echo '</p></div>';
-	}
-
 	return array('url' => $guessurl, 'user_id' => $user_id, 'password' => $user_password, 'password_message' => $message);
 }
 ?>
